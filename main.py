14363e8ee578fa0549c0655abab90f9768afff37
HimnashuAmlavad <amlavadhimanshu@gmail.com>
2024-12-30 13:48:47 +0530

    game function added

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-
-from flask import Flask, render_template , session
+from flask import Flask, render_template , session , redirect , request
+import random
 
 app = Flask(__name__)
 app.secret_key = 'Rock_Paper_scissors'
+connection = []
 
-@app.route('/' , methods = ['POST', 'GET'])
+@app.route('/' , methods = ['GET'])
 def home():
-    return render_template('Game_page.html')
+    if (request.method == 'GET') and (request.args.get('id') in connection) and (request.args.get('id')):
+        if user_input := request.args.get('choice'):
+            dic = { 1 : "rock" , 2 : "paper" , 3 : "scissor"}
+        
+            cpu_input = lambda: random.randint(1, 3)
+            cpu_ans = dic[cpu_input()]
+          #------game part------
+            if((user_input == 'rock' and cpu_ans =='scissor') or (user_input == 'paper' and cpu_ans == 'rock') or (user_input == 'scissor' and cpu_ans == 'paper')):
+                print(f"Player win\n {cpu_ans}")
+            elif((user_input == 'scissor' and cpu_ans =='rock') or (user_input == 'rock' and cpu_ans == 'paper') or (user_input == 'paper' and cpu_ans == 'scissor')):
+                print(f"Player lose\n{cpu_ans}")
+            elif(user_input == cpu_ans):
+                print(f"Draw!!\n{cpu_ans}")
+            else:
+                print("WARNING : Invalid Choice!!!!")
+   
+            return render_template('Game_page.html', id=request.remote_addr)
+        else:
+            return render_template('Game_page.html', id=request.remote_addr)
+    else:
+        if request.remote_addr not in connection:
+            connection.append(request.remote_addr)
+        return redirect(f'/?id={request.remote_addr}')
 
 if __name__ =="__main__":
     app.run(debug=True)    
